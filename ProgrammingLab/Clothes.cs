c74e9bb615aaf8cd13345b0907ac3f9b45bf171f
Vlad <Vsoilol1701@gmail.com>
2021-03-25 21:05:03 +0300

    Create project

---
+++ ProgrammingLab/Clothes.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingLab
+{
+    public class Clothes
+    {
+        private readonly string fabric;//Модель ткани
+        private int width;//Ширина ткани
+        private int fabricNorm;//Норма расхода
+
+        public int Width
+        {
+            get { return width; }
+            set
+            {
+                if (value >= 0)
+                {
+                    width = value;
+                }
+            }
+        }
+
+        public int FabricNorm
+        {
+            get { return fabricNorm; }
+            set
+            {
+                if (value >= 0)
+                {
+                    fabricNorm = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Вывод информации
+        /// </summary>
+        /// <returns>Информация об одежде</returns>
+        public string OutputInformation() =>
+            $"Модель ткани {fabric}, ширина ткани {width} м, норма расхода {fabricNorm} м";
+
+        public Clothes()
+        {
+            fabric = "None";
+            width = 0;
+            fabricNorm = 0;
+        }
+
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="fabric">Модель</param>
+        /// <param name="width">Ширина</param>
+        /// <param name="fabricNorm">Норма расхода</param>
+        public Clothes(string fabric, int width, int fabricNorm)
+        {
+            this.fabric = fabric;
+            this.width = width;
+            this.fabricNorm = fabricNorm;
+        }
+
+        /// <summary>
+        /// Метод который подсчитывает расход ткани
+        /// </summary>
+        /// <returns>Расход ткани</returns>
+        public int FabricConsumption() => (2 + width) * fabricNorm;
+    }
+}
--- ProgrammingLab/Program.cs
+++ ProgrammingLab/Program.cs
@@ -6,7 +6,25 @@ namespace ProgrammingLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Clothes clothesWithoutParameters = new Clothes();
+            Clothes clothesWithParameters = new Clothes("Кожа", 20, 10);
+
+            Console.WriteLine("Одежа с конструктором без параметров:");
+            Console.WriteLine($"Данные - {clothesWithoutParameters.OutputInformation()}");
+            Console.WriteLine($"Расход ткани - {clothesWithoutParameters.FabricConsumption()}");
+
+
+            Console.WriteLine("\nОдежа с конструктором с параметрами:");
+            Console.WriteLine($"Данные - {clothesWithParameters.OutputInformation()}");
+            Console.WriteLine($"Расход ткани - {clothesWithParameters.FabricConsumption()}");
+
+            Console.Write("\nВведите ширину ткани: ");
+            clothesWithParameters.Width = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите норму расхода ткани: ");
+            clothesWithParameters.FabricNorm = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Полученный расход ткани - {clothesWithParameters.FabricConsumption()}");
         }
     }
 }
