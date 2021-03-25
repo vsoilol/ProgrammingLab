dd5b6f4a2614390040821a8baa86a7311a0268e7
Vlad <Vsoilol1701@gmail.com>
2021-03-26 00:23:13 +0300

    Create

--- ProgrammingLab/Program.cs
+++ ProgrammingLab/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 
-namespace ProgrammingLab
+namespace Task1
 {
     class Program
     {
+        const char Colon = ':';
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string testString = "Hello:ufhy7ergferf:eryfgeryuifn:Bye";
+
+            Console.WriteLine($"Исходная строка {testString}");
+
+            Console.WriteLine($"Строка до первого двоеточия: {TakeBeforeColon(testString)}");
+
+            Console.WriteLine($"Строка после последнего двоеточия: {TakeAfterColon(testString)}");
+        }
+
+        static private string TakeBeforeColon(string text)
+        {
+            int firstIndexColon = text.IndexOf(Colon);
+
+            return text.Substring(0, firstIndexColon);
+        }
+
+        static private string TakeAfterColon(string text)
+        {
+            int lastIndexColon = text.LastIndexOf(Colon);
+
+            return text.Remove(0, lastIndexColon + 1);
         }
     }
 }
---
+++ Task2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder firstTask = new StringBuilder("1Hello36273447W6or67ldHow76AreYou23");
+
+            DeleteNumbers(ref firstTask);
+            Console.WriteLine(firstTask);
+
+            StringBuilder secondTask = new StringBuilder("OMGGG!!!!!!!aaattttkkkklaaaa");
+
+            DeleteLongestRecurringChar(ref secondTask);
+            Console.WriteLine(secondTask);
+        }
+
+        static private void DeleteNumbers(ref StringBuilder stringBuilder)
+        {
+            for (int i = 0; i < stringBuilder.Length; i++)
+            {
+                if (Char.IsDigit(stringBuilder[i]))
+                {
+                    stringBuilder.Remove(i, 1);
+                    i--;
+                }
+            }
+        }
+
+        static private void DeleteLongestRecurringChar(ref StringBuilder stringBuilder)
+        {
+            char longestChar = stringBuilder[0];
+
+            int maxRepeat = 1;
+            int repeat = 0;
+            int finiteIndex = 0;
+
+            for (int i = 0; i < stringBuilder.Length - 1; i++)
+            {
+                if(stringBuilder[i] == stringBuilder[i + 1])
+                {
+                    repeat++;
+
+                }
+                else
+                {
+                    if(repeat > maxRepeat)
+                    {
+                        longestChar = stringBuilder[i];
+                        finiteIndex = i;
+                        maxRepeat = repeat;
+                    }
+                    repeat = 1;
+                }
+            }
+
+            stringBuilder.Remove(finiteIndex - maxRepeat + 1, maxRepeat);
+        }
+    }
+}
