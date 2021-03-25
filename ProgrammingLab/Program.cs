b844dd5f3a2304e3afc7090a54ae771d41a6a6d2
Vlad <Vsoilol1701@gmail.com>
2021-03-25 21:02:42 +0300

    Create project

--- ProgrammingLab/Program.cs
+++ ProgrammingLab/Program.cs
@@ -6,7 +6,20 @@ namespace ProgrammingLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = -10; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} - {RaiseDegree(i)}");
+            }
+        }
+
+        /// <summary>
+        /// Метод который возводит число в квадрат если число больше нуля и в куб если меньше
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        static public int RaiseDegree(int number)
+        {
+            return (number >= 0) ? number * number : number * number * number;
         }
     }
 }
