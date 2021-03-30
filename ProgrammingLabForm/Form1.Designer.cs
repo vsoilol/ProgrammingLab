ef2ab98e21479d13cf64da7d3d218956d8740918
Vlad <Vsoilol1701@gmail.com>
2021-03-31 01:31:06 +0300

    Create

---
+++ ProgrammingLabForm/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace ProgrammingLabForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxMain = new System.Windows.Forms.TextBox();
+            this.buttonFill = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxMain
+            // 
+            this.textBoxMain.Location = new System.Drawing.Point(12, 12);
+            this.textBoxMain.Name = "textBoxMain";
+            this.textBoxMain.Size = new System.Drawing.Size(214, 22);
+            this.textBoxMain.TabIndex = 0;
+            // 
+            // buttonFill
+            // 
+            this.buttonFill.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonFill.Location = new System.Drawing.Point(12, 40);
+            this.buttonFill.Name = "buttonFill";
+            this.buttonFill.Size = new System.Drawing.Size(104, 41);
+            this.buttonFill.TabIndex = 1;
+            this.buttonFill.Text = "Заполнить";
+            this.buttonFill.UseVisualStyleBackColor = true;
+            this.buttonFill.Click += new System.EventHandler(this.buttonFill_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonClear.Location = new System.Drawing.Point(122, 40);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(104, 41);
+            this.buttonClear.TabIndex = 2;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 98);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonFill);
+            this.Controls.Add(this.textBoxMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxMain;
+        private System.Windows.Forms.Button buttonFill;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
