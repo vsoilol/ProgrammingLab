1d12899654c99706b64cc8f948b629bb0f7811d3
Vlad <Vsoilol1701@gmail.com>
2021-03-31 01:10:07 +0300

    Create

--- ProgrammingLab/Program.cs
+++ ProgrammingLab/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ProgrammingLab
 {
@@ -6,7 +8,27 @@ namespace ProgrammingLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string serializeFile = "Film.dat";
+            Random budget = new Random();
+
+            Film film1 = new Film("Film1", new DateTime(2021, 1, 1, 15, 0, 0), new TimeSpan(1, 20, 0), "Сказка", budget.Next(300, 5000));
+
+            Console.WriteLine($"Начальный объект\n{film1}");
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream(serializeFile, FileMode.Create))
+            {
+                formatter.Serialize(fs, film1);
+            }
+
+            Film film2;
+            using (FileStream fs = new FileStream(serializeFile, FileMode.Open))
+            {
+                film2 = (Film)formatter.Deserialize(fs);
+            }
+
+            Console.WriteLine($"\nПосле десериализации\n{film2}");
         }
     }
 }
